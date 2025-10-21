37bc3f57dd579b0d8fbe4296c927bb5a0d9aa1fc
Debadipta Sahu <debssahu123@gmail.com>
2025-10-21 15:02:14 +0530

    added sort of the array

---
+++ DSA_30.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class DSA_30 {
+    
+    public static int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
+
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+    public static void main(String args[]){
+        int[] arr = {29, 12, 13, 32, 5};
+        int[] result = sortArray(arr);
+        System.out.println(Arrays.toString(result));
+    }
+}
+
